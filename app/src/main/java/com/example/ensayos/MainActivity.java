831a76fcca1192e9512558b9fcb3165b5ad57b70
victoreramirez <vzt50000@gmail.com>
2021-02-21 01:38:23 -0600

    1

--- app/src/main/java/com/example/ensayos/MainActivity.java
+++ app/src/main/java/com/example/ensayos/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.ensayos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
